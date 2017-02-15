e598a5735aaca88709524140a06c6970368d11a1
sashaaero <sashaaero@yandex.ru>
2017-02-15 04:06:58 +0300

    Done

--- src/Frame.java
+++ src/Frame.java
@@ -4,24 +4,29 @@ import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
 import java.io.File;
 
-public class Frame extends JFrame{
+public class Frame extends JFrame
+{
     private Game game;
     private JLabel[][] labelsField;
 
-    public Frame(Game game){
+    public Frame(Game game)
+    {
         this.game = game;
         initGUI();
     }
 
-    private void initGUI(){
+    private void initGUI()
+    {
         this.setTitle("2048");
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // Чтобы программа заканчивалась по нажатию на крестик
         this.setResizable(false); // Запрет на изменение размера окна
         this.setSize(450, 450);
         this.setLayout(new GridLayout(game.size, game.size));
         labelsField = new JLabel[game.size][game.size];
-        for(int i = 0; i < game.size; i++){
-            for(int j = 0; j < game.size; j++){
+        for(int i = 0; i < game.size; i++)
+        {
+            for(int j = 0; j < game.size; j++)
+            {
                 labelsField[i][j] = new JLabel();
                 labelsField[i][j].setForeground(Color.white);
                 labelsField[i][j].setHorizontalAlignment(JLabel.CENTER);
@@ -30,25 +35,27 @@ public class Frame extends JFrame{
         }
         updateField();
         this.setVisible(true);
-        this.addKeyListener(new KeyAdapter() {
+        this.addKeyListener(new KeyAdapter()
+        {
             @Override
-            public void keyReleased(KeyEvent e) {
+            public void keyReleased(KeyEvent e)
+            {
                 super.keyReleased(e);
                 game.move(e.getKeyCode());
+                updateField();
             }
         });
     }
 
-    public void updateField(){
-        File file = new File("img/" + Integer.toString(128) + ".png");
-        System.out.println(file.getAbsolutePath());
-        for(int i = 0; i < game.size; i++){
-            for(int j = 0; j < game.size; j++){
+    public void updateField()
+    {
+        for(int i = 0; i < game.size; i++)
+        {
+            for(int j = 0; j < game.size; j++)
+            {
                 int value = game.field[i][j];
                 labelsField[i][j].setIcon(new ImageIcon("img/" + Integer.toString(value) + ".png"));
             }
         }
     }
-
-
 }
--- src/Game.java
+++ src/Game.java
@@ -1,32 +1,41 @@
-import java.util.Arrays;
-
 import static java.awt.event.KeyEvent.*;
 
-public class Game {
+public class Game
+{
     public final int size = 4;
     public int[][] field = new int[size][size];
     public boolean over = false;
 
-    public Game(){
-        for (int  i = 0; i < size; i++){
-            for(int j = 0; j < size; j++){
-                field[i][j] = 128;
+    public Game()
+    {
+        for (int  i = 0; i < size; i++)
+        {
+            for(int j = 0; j < size; j++)
+            {
+                field[i][j] = 0;
             }
         }
     }
 
-    int getRandomNum(){
-        if (Math.random() > 0.9)
+    int getRandomNum()
+    {
+        if (Math.random() < 0.9)
+        {
             return 2;
+        }
         return 4;
     }
 
-    int getRandomEmptyCell(){
+    int getRandomEmptyCell()
+    {
         int[] cells = new int[size * size];
         int realSize = 0;
-        for (int i = 0; i < size; i++){
-            for(int j = 0; j < size; j++){
-                if(field[i][j] == 0){
+        for (int i = 0; i < size; i++)
+        {
+            for(int j = 0; j < size; j++)
+            {
+                if(field[i][j] == 0)
+                {
                     cells[realSize] = i * 10 + j;
                     realSize++;
                 }
@@ -38,7 +47,8 @@ public class Game {
         return cells[peek];
     }
 
-    void create(){
+    void create()
+    {
         int xy = this.getRandomEmptyCell();
         int i = xy / 10;
         int j = xy % 10;
@@ -46,84 +56,113 @@ public class Game {
         this.field[i][j] = val;
     }
 
-    void move(int direction){
+    void move(int direction)
+    {
         if (this.over){return;}
-        int[][] old_field = Arrays.copyOf(field, field.length);
+        int[][] old_field = new int[size][size];
+        for(int i = 0; i < size; i++)
+        {
+            for(int j = 0; j < size; j++)
+            {
+                old_field[i][j] = field[i][j];
+            }
+        }
 
-        switch (direction){
+        switch (direction)
+        {
             case VK_UP:
-                for(int row = 1; row < size; row++){
-                    for(int col = 0; col < size; col++){
-                        if(field[row][col] == 0){
+                for (int row = 1; row < size; row++)
+                {
+                    for (int col = 0; col < size; col++)
+                    {
+                        if (field[row][col] == 0)
+                        {
                             continue;
                         }
                         int newRow = row - 1;
-                        while(newRow >= 0 && field[newRow][col] == 0){
-                            newRow -= 1;
+                        while (newRow >= 0 && field[newRow][col] == 0)
+                        {
+                            newRow--;
                         }
 
-                        if(newRow == -1){
+                        if (newRow == -1)
+                        {
                             field[0][col] = field[row][col];
                             field[row][col] = 0;
                         }
-                        else if(field[newRow][col] == field[row][col]){
+                        else if (field[newRow][col] == field[row][col])
+                        {
                             field[newRow][col] *= 2;
                             field[row][col] = 0;
                         }
-                        else{
+                        else {
                             int val = field[row][col];
                             field[row][col] = 0;
                             field[newRow + 1][col] = val;
                         }
                     }
                 }
-
+                break;
             case VK_RIGHT:
-                for(int col = size - 2; col >= 0; col--){
-                    for(int row = 0; row < size; row++){
-                        if(field[row][col] == 0){
+                for (int col = size - 2; col >= 0; col--)
+                {
+                    for (int row = 0; row < size; row++)
+                    {
+                        if (field[row][col] == 0)
+                        {
                             continue;
                         }
                         int newCol = col + 1;
-                        while(newCol < size && field[row][newCol] == 0){
-                            newCol += 1;
+                        while (newCol < size && field[row][newCol] == 0)
+                        {
+                            newCol++;
                         }
 
-                        if(newCol == size){
+                        if (newCol == size) {
                             field[row][size - 1] = field[row][col];
                             field[row][col] = 0;
                         }
-                        else if(field[row][newCol] == field[row][col]){
+                        else if (field[row][newCol] == field[row][col])
+                        {
                             field[row][newCol] *= 2;
                             field[row][col] = 0;
                         }
-                        else{
+                        else
+                        {
                             int val = field[row][col];
                             field[row][col] = 0;
                             field[row][newCol - 1] = val;
                         }
                     }
                 }
+                break;
             case VK_DOWN:
-                for(int row = size - 2; row >= 0; row--){
-                    for(int col = 0; col < size; col++){
-                        if(field[row][col] == 0){
+                for (int row = size - 2; row >= 0; row--)
+                {
+                    for (int col = 0; col < size; col++)
+                    {
+                        if (field[row][col] == 0)
+                        {
                             continue;
                         }
                         int newRow = row + 1;
-                        while(newRow < size && field[newRow][col] == 0){
-                            newRow += 1;
+                        while (newRow < size && field[newRow][col] == 0)
+                        {
+                            newRow++;
                         }
 
-                        if(newRow == size){
+                        if (newRow == size)
+                        {
                             field[size - 1][col] = field[row][col];
                             field[row][col] = 0;
                         }
-                        else if(field[newRow][col]== field[row][col]){
+                        else if (field[newRow][col] == field[row][col])
+                        {
                             field[newRow][col] *= 2;
                             field[row][col] = 0;
                         }
-                        else{
+                        else
+                        {
                             int val = field[row][col];
                             field[row][col] = 0;
                             field[newRow - 1][col] = val;
@@ -131,52 +170,68 @@ public class Game {
                     }
 
                 }
-
+                break;
             case VK_LEFT:
-                for(int col = 1; col < size; col++){
-                    for(int row = 0; row < size; row++){
-                        if(field[row][col] == 0){
+                for (int col = 1; col < size; col++)
+                {
+                    for (int row = 0; row < size; row++)
+                    {
+                        if (field[row][col] == 0)
+                        {
                             continue;
                         }
 
                         int newCol = col - 1;
-                        while(newCol > 0 && field[row][newCol] == 0){
-                            newCol -= 1;
+                        while (newCol >= 0 && field[row][newCol] == 0)
+                        {
+                            newCol--;
                         }
 
-                        if(newCol == size){
-                            field[row][size + 1] = field[row][col];
+                        if (newCol == -1)
+                        {
+                            field[row][newCol + 1] = field[row][col];
                             field[row][col] = 0;
                         }
-                        else if (field[row][newCol] == field[row][col]) {
+                        else if (field[row][newCol] == field[row][col])
+                        {
                             field[row][newCol] *= 2;
                             field[row][col] = 0;
                         }
-                        else {
+                        else
+                        {
                             int val = field[row][col];
                             field[row][col] = 0;
                             field[row][newCol + 1] = val;
                         }
                     }
                 }
-
-            for(int i = 0; i < size; i++){
-                for(int j = 0; j < size; j++){
-                    if(field[i][j] != old_field[i][j]){
-                        create();
-                    }
-                    if(field[i][j] == 2048){
-                        over = true;
-                        break;
-                    }
+                break;
+        }
+        boolean changed = false;
+        for(int i = 0; i < size; i++)
+        {
+            for(int j = 0; j < size; j++)
+            {
+                if(field[i][j] != old_field[i][j])
+                {
+                    changed = true;
+                }
+                if(field[i][j] == 2048)
+                {
+                    over = true;
+                    return;
                 }
             }
         }
-
+        if (changed)
+        {
+            create();
+        }
     }
 
-
-    void start(){
-        getRandomEmptyCell();
+    void start()
+    {
+        create();
+        create();
     }
 }
